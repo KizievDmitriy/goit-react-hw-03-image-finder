e7b55415f5fac1bb62d44f894b6aaa97171dbcf6
KizievDmitriy <kizievdmitrii@gmail.com>
2022-07-15 22:19:09 +0300

    add loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,11 @@
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import s from './Loader.module.css';
-import { TailSpin } from 'react-loader-spinner'
-
-
+import { TailSpin } from  'react-loader-spinner'
 export const Loader = () => {
   return (
-    <div className={s.Loader}>
-      <TailSpin
-          color="#3f51b5"
-          height={100}
-          width={100}
-        ariaLabel="loading" />
-    </div>
+    <TailSpin
+    height="100"
+    width="100"
+    color='#3f51b5'
+    ariaLabel='loading'
+  />
   );
 };
